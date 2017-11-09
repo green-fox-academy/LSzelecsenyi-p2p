febcb0c2b1fa8961d5da96132b1dc9b96a8d2bd5
LSzelecsenyi <szelelaci@gmail.com>
2017-11-09 18:11:43 +0100

    firstMessage

--- src/main/java/com/greenfox/p2pchat/controller/RestCont.java
+++ src/main/java/com/greenfox/p2pchat/controller/RestCont.java
@@ -4,19 +4,21 @@ import com.greenfox.p2pchat.model.Message;
 import com.greenfox.p2pchat.repository.ChatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping({"", "/"})
 public class RestCont {
 
     @Autowired
     private ChatRepository chatRepository;
 
     @PostMapping("/savemessage")
-    public Message postMessage(@RequestParam("message") String message) {
+    public String postMessage(@RequestParam("message") String message) {
         Message firstMessage = new Message(message);
         chatRepository.save(firstMessage);
-        return firstMessage;
+        return "redirect:/";
     }
 }
--- src/main/java/com/greenfox/p2pchat/model/Message.java
+++ src/main/java/com/greenfox/p2pchat/model/Message.java
@@ -3,10 +3,12 @@ package com.greenfox.p2pchat.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Message {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
