c63664664314ccb4a4dc5cadf19f404774098d02
LSzelecsenyi <szelelaci@gmail.com>
2017-11-09 17:31:26 +0100

    firstMessage

--- src/main/java/com/greenfox/p2pchat/repository/ChatRepository.java
+++ src/main/java/com/greenfox/p2pchat/repository/ChatRepository.java
@@ -1,6 +1,7 @@
 package com.greenfox.p2pchat.repository;
 
+import com.greenfox.p2pchat.model.Message;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ChatRepository extends CrudRepository {
+public interface ChatRepository extends CrudRepository<Message, Long> {
 }
