f77d05b0dcff45f7b71ec4997dbe08032b6b8849
LSzelecsenyi <szelelaci@gmail.com>
2017-11-09 17:34:25 +0100

    firstMessage

--- src/main/java/com/greenfox/p2pchat/controller/RestCont.java
+++ src/main/java/com/greenfox/p2pchat/controller/RestCont.java
@@ -14,9 +14,9 @@ public class RestCont {
     private ChatRepository chatRepository;
 
     @PostMapping("/savemessage")
-    public String postMessage(@RequestParam("message") String message) {
+    public Message postMessage(@RequestParam("message") String message) {
         Message firstMessage = new Message(message);
         chatRepository.save(firstMessage);
-        return "redirect:/chat";
+        return firstMessage;
     }
 }
